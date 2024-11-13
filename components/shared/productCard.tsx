3ae94c876717209738705e02bee072210d35685d
maximkashkevich <kashkevich.maxim@gmail.com>
2024-11-13 19:44:42 +0300

    fixed bugs from versel

--- components/shared/productCard.tsx
+++ components/shared/productCard.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Title } from "./index";
 import { Button } from "../ui";
 import { Plus } from "lucide-react";
+import Image from "next/image";
 
 interface Props {
   id: number;
@@ -22,10 +23,10 @@ export const ProductCard: React.FC<Props> = ({
   className,
 }) => {
   return (
-    <div>
+    <div className={className}>
       <Link href={`/product/${id}`}>
         <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
-          <img className="w-[215px] h-[215px]" src={imageUrl} alt="Logo" />
+          <Image className="w-[215px] h-[215px]" src={imageUrl} alt="Logo" />
         </div>
         <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
         <p className="text-sm text-gray-400">
