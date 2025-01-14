365bfe5833f2d7d20e0ca6660f83be60d675d999
MaximKashkevich <kashkevich.maxim@gmail.com>
2025-01-14 19:25:16 +0300

    delete console.logs

--- components/shared/productCard.tsx
+++ components/shared/productCard.tsx
@@ -1,48 +1,47 @@
-import React from "react";
-import Link from "next/link";
-import { Title } from "./index";
-import { Button } from "../ui";
-import { Plus } from "lucide-react";
+import { Plus } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+import { Button } from '../ui'
+import { Title } from './index'
 
 interface Props {
-  id: number;
-  name: string;
-  price: number;
-  imageUrl: string;
-  // ingredients: Ingredient[];
-  className?: string;
+	id: number
+	name: string
+	price: number
+	imageUrl: string
+	// ingredients: Ingredient[];
+	className?: string
 }
 
 export const ProductCard: React.FC<Props> = ({
-  id,
-  name,
-  price,
-  imageUrl,
-  //   ingredients,
-  className,
+	id,
+	name,
+	price,
+	imageUrl,
+	//   ingredients,
+	className,
 }) => {
-  console.log(price);
-  return (
-    <div className={className}>
-      <Link href={`/product/${id}`}>
-        <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
-          <img width={215} height={215} src={imageUrl} alt="Logo" />
-        </div>
-        <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
-        <p className="text-sm text-gray-400">
-          Цыпленок, моцарелла, сыры чедер и пормезан, сырный соус, томаты
-        </p>
-      </Link>
-      <div className="flex justify-between items-center mt-4">
-        <span className="text-[20px]">
-          от <b>{price} ₽</b>
-        </span>
+	return (
+		<div className={className}>
+			<Link href={`/product/${id}`}>
+				<div className='flex justify-center p-6 bg-secondary rounded-lg h-[260px]'>
+					<img width={215} height={215} src={imageUrl} alt='Logo' />
+				</div>
+				<Title text={name} size='sm' className='mb-1 mt-3 font-bold' />
+				<p className='text-sm text-gray-400'>
+					Цыпленок, моцарелла, сыры чедер и пормезан, сырный соус, томаты
+				</p>
+			</Link>
+			<div className='flex justify-between items-center mt-4'>
+				<span className='text-[20px]'>
+					от <b>{price} ₽</b>
+				</span>
 
-        <Button variant="secondary" className="text-base font-bold">
-          <Plus size={20} className="mr-1" />
-          Добавить
-        </Button>
-      </div>
-    </div>
-  );
-};
+				<Button variant='secondary' className='text-base font-bold'>
+					<Plus size={20} className='mr-1' />
+					Добавить
+				</Button>
+			</div>
+		</div>
+	)
+}
--- components/shared/productsGroupList.tsx
+++ components/shared/productsGroupList.tsx
@@ -1,56 +1,53 @@
-"use client";
+'use client'
 
-import React, { useEffect } from "react";
-import { Title } from "./title";
-import { cn } from "../../lib/utils";
-import { ProductCard } from "./productCard";
-import { useIntersection } from "react-use";
-import { useRef } from "react";
+import React, { useEffect, useRef } from 'react'
+import { useIntersection } from 'react-use'
+import { cn } from '../../lib/utils'
+import { ProductCard } from './productCard'
+import { Title } from './title'
 
 interface Props {
-  title: string;
-  items: any[];
-  categoryId: number;
-  className?: string;
-  listClassName?: string;
+	title: string
+	items: any[]
+	categoryId: number
+	className?: string
+	listClassName?: string
 }
 
 export const ProductsGroupList: React.FC<Props> = ({
-  title,
-  items,
-  listClassName,
-  categoryId,
-  className,
+	title,
+	items,
+	listClassName,
+	categoryId,
+	className,
 }) => {
-  const intersectionRef = useRef(null);
-  const intersection = useIntersection(intersectionRef, {
-    threshold: 0.4,
-  });
+	const intersectionRef = useRef(null)
+	const intersection = useIntersection(intersectionRef, {
+		threshold: 0.4,
+	})
 
-  useEffect(() => {
-    if (intersection?.isIntersecting) {
-      console.log(title, categoryId);
-    }
-  }, [intersection?.isIntersecting]);
+	useEffect(() => {
+		if (intersection?.isIntersecting) {
+			console.log(title, categoryId)
+		}
+	}, [intersection?.isIntersecting])
 
-  console.log(items);
-
-  return (
-    <div className={className} id={title} ref={intersectionRef}>
-      <Title text={title} size="lg" className="font-extrabold mb-5" />
-      <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
-        {items.map((product, i) => (
-          <ProductCard
-            key={product.id}
-            id={product.id}
-            name={product.name}
-            imageUrl={product.imageUrl}
-            price={product.items[0].price}
-            // price={product.price}
-            // ingredients={product.ingredients}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className={className} id={title} ref={intersectionRef}>
+			<Title text={title} size='lg' className='font-extrabold mb-5' />
+			<div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+				{items.map((product, i) => (
+					<ProductCard
+						key={product.id}
+						id={product.id}
+						name={product.name}
+						imageUrl={product.imageUrl}
+						price={product.items[0].price}
+						// price={product.price}
+						// ingredients={product.ingredients}
+					/>
+				))}
+			</div>
+		</div>
+	)
+}
