f22d4238dde6368a9b3d4fc3cb1dd88baca000d4
MaximKashkevich <kashkevich.maxim@gmail.com>
2025-01-14 19:12:43 +0300

    fixed bugs

--- app/(root)/products/[id]/page.tsx
+++ app/(root)/products/[id]/page.tsx
@@ -1,36 +1,35 @@
-import Container from "postcss/lib/container";
-import { prisma } from "../../../../prisma/prisma-client";
-import { notFound } from "next/navigation";
+import { notFound } from 'next/navigation'
+import { prisma } from '../../../../prisma/prisma-client'
 
 export default async function ProductPage({
-  params: { id },
+	params: { id },
 }: {
-  params: { id: string };
+	params: { id: string }
 }) {
-  const product = await prisma.product.findFirst({
-    where: { id: Number(id) },
-    include: {
-      ingredients: true,
-      category: {
-        include: {
-          products: {
-            include: {
-              items: true,
-            },
-          },
-        },
-      },
-      items: true,
-    },
-  });
+	const product = await prisma.product.findFirst({
+		where: { id: Number(id) },
+		include: {
+			ingredients: true,
+			category: {
+				include: {
+					products: {
+						include: {
+							items: true,
+						},
+					},
+				},
+			},
+			items: true,
+		},
+	})
 
-  if (!product) {
-    return notFound();
-  }
+	if (!product) {
+		return notFound()
+	}
 
-  return (
-    <Container className="flex flex-col my-10">
-      {/* <ProductForm product={product} /> */}
-    </Container>
-  );
+	// return (
+	//   // <Container className="flex flex-col my-10">
+	//   //   {/* <ProductForm product={product} /> */}
+	//   // </Container>
+	// );
 }
