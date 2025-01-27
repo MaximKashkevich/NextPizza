c24c75d81ad73cd5075d720dff5e7fe411b25e4b
MaximKashkevich <kashkevich.maxim@gmail.com>
2025-01-27 21:59:30 +0300

    fix bugs from vercel

--- app/(root)/@mod/(.)product/[id]/page.tsx
+++ app/(root)/@mod/(.)product/[id]/page.tsx
@@ -2,19 +2,24 @@ import { notFound } from 'next/navigation'
 import { ChooseProductModal } from '../../../../../components/shared'
 import { prisma } from '../../../../../prisma/prisma-client'
 
-export default async function ProductModalPage({
-	params,
-}: {
-	params: { id: string | undefined }
-}) {
-	// Проверяем, есть ли id
-	if (!params.id) {
+interface PageProps {
+	params: {
+		id: string
+	}
+}
+
+export default async function ProductModalPage({ params }: PageProps) {
+	const { id } = params // Деструктурируем id из params
+
+	// Проверяем, если id не определен (хотя это не должно происходить в Next.js)
+	if (!id) {
 		return notFound()
 	}
 
+	// Запрос к базе данных
 	const product = await prisma.product.findFirst({
 		where: {
-			id: Number(params.id),
+			id: Number(id),
 		},
 		include: {
 			ingredients: true,
@@ -22,9 +27,11 @@ export default async function ProductModalPage({
 		},
 	})
 
+	// Проверяем на наличие продукта в базе данных
 	if (!product) {
 		return notFound()
 	}
 
+	// Возвращаем компонент с продуктом
 	return <ChooseProductModal product={product} />
 }
