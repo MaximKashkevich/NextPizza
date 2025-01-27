9a98cb15913c13e626e20d668fbbb13b9128bd07
MaximKashkevich <kashkevich.maxim@gmail.com>
2025-01-27 21:56:29 +0300

    fix bugs2

--- app/(root)/@mod/(.)product/[id]/page.tsx
+++ app/(root)/@mod/(.)product/[id]/page.tsx
@@ -3,13 +3,18 @@ import { ChooseProductModal } from '../../../../../components/shared'
 import { prisma } from '../../../../../prisma/prisma-client'
 
 export default async function ProductModalPage({
-	params: { id },
+	params,
 }: {
-	params: { id: string }
+	params: { id: string | undefined }
 }) {
+	// Проверяем, есть ли id
+	if (!params.id) {
+		return notFound()
+	}
+
 	const product = await prisma.product.findFirst({
 		where: {
-			id: Number(id),
+			id: Number(params.id),
 		},
 		include: {
 			ingredients: true,
