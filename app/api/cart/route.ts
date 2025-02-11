621c173b9a9bc0fe34ecd16ce7da1600402fa953
MaximKashkevich <kashkevich.maxim@gmail.com>
2025-02-11 14:32:12 +0300

    post on cart

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -2,6 +2,8 @@ import crypto from 'crypto'
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '../../../prisma/prisma-client'
 import { findOrCreateCart } from '../../../shared/lib/find-or-create-cart'
+import { updateCartTotalAmount } from '../../../shared/lib/update-cart-total-amount'
+import { CreateCartItemValues } from '../../../shared/services/dto/cart.dto'
 
 export async function GET(req: NextRequest) {
 	try {
@@ -55,8 +57,52 @@ export async function POST(req: NextRequest) {
 		}
 
 		const userCart = await findOrCreateCart(token)
+
+		const data = (await req.json()) as CreateCartItemValues
+
+		const findCartItem = await prisma.cartItem.findFirst({
+			where: {
+				cartId: userCart.id,
+				productItemId: data.productItemId,
+				ingredients: {
+					every: {
+						id: { in: data.ingredients },
+					},
+				},
+			},
+		})
+
+		// Если товар был найден, делаем +1
+		if (findCartItem) {
+			await prisma.cartItem.update({
+				where: {
+					id: findCartItem.id,
+				},
+				data: {
+					quantity: findCartItem.quantity + 1,
+				},
+			})
+		} else {
+			await prisma.cartItem.create({
+				data: {
+					cartId: userCart.id,
+					productItemId: data.productItemId,
+					quantity: 1,
+					ingredients: { connect: data.ingredients?.map(id => ({ id })) },
+				},
+			})
+		}
+
+		const updatedUserCart = await updateCartTotalAmount(token)
+
+		const resp = NextResponse.json(updatedUserCart)
+		resp.cookies.set('cartToken', token)
+		return resp
 	} catch (error) {
-		console.log('[CART_POST] Server error', error)
+		console.log(
+			'[CART_POST] Server error',
+			error ? JSON.stringify(error) : 'Unknown error'
+		)
 		return NextResponse.json(
 			{ message: 'Не удалось создать корзину' },
 			{ status: 500 }
